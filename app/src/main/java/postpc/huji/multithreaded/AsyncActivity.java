a521d1dbf80c618a363194e913bd1d8c9945b0c2
oBit91 <ohad.beltzer@mail.huji.ac.il>
2018-05-15 23:23:58 +0300

    initializing

---
+++ app/src/main/java/postpc/huji/multithreaded/AsyncActivity.java
@@ -0,0 +1,13 @@
+package postpc.huji.multithreaded;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AsyncActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async);
+    }
+}
